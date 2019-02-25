8b2083fcaade1ae081edbbaaeddf979545f3b8d2
Philip Wester <Philip.Wester@outlook.com>
2019-02-25 14:44:01 +0100

    #4 reduced ccn in CustomEditText.java

--- app/src/main/java/fr/free/nrw/commons/ui/widget/CustomEditText.java
+++ app/src/main/java/fr/free/nrw/commons/ui/widget/CustomEditText.java
@@ -52,6 +52,57 @@ public class CustomEditText extends EditText {
         super.setCompoundDrawables(left, top, right, bottom);
     }
 
+    private boolean isDrawableRight(Rect bounds, MotionEvent event){
+        bounds = null;
+        bounds = drawableRight.getBounds();
+
+        int x, y;
+        int extraTapArea = 13;
+
+                /*
+                  IF USER CLICKS JUST OUT SIDE THE RECTANGLE OF THE DRAWABLE
+                  THAN ADD X AND SUBTRACT THE Y WITH SOME VALUE SO THAT AFTER
+                  CALCULATING X AND Y CO-ORDINATE LIES INTO THE DRAWBABLE
+                  BOUND. - this process help to increase the tappable area of
+                  the rectangle.
+                 */
+        x = (int) (actionX + extraTapArea);
+        y = (int) (actionY - extraTapArea);
+
+                /*
+                  Since this is right drawable subtract the value of x from the width
+                  of view. so that width - tappedarea will result in x co-ordinate in drawable bound.
+                 */
+        x = getWidth() - x;
+
+        /*x can be negative if user taps at x co-ordinate just near the width.
+         * e.g views width = 300 and user taps 290. Then as per previous calculation
+         * 290 + 13 = 303. So subtract X from getWidth() will result in negative value.
+         * So to avoid this add the value previous added when x goes negative.
+         */
+
+        if (x <= 0) {
+            x += extraTapArea;
+        }
+
+        /* If result after calculating for extra tappable area is negative.
+         * assign the original value so that after subtracting
+         * extra tapping area value doesn't go into negative value.
+         */
+
+        if (y <= 0)
+            y = actionY;
+
+        // If drawble bounds contains the x and y points then move ahead.
+        if (bounds.contains(x, y) && clickListener != null) {
+            clickListener
+                    .onClick(DrawableClickListener.DrawablePosition.RIGHT);
+            event.setAction(MotionEvent.ACTION_CANCEL);
+            return false;
+        }
+        return super.onTouchEvent(event);
+
+    }
     /**
      * Fires the appropriate drawable click listener on touching the icon
      * @param event
@@ -59,7 +110,7 @@ public class CustomEditText extends EditText {
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Rect bounds;
+        Rect bounds = null;
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             actionX = (int) event.getX();
             actionY = (int) event.getY();
@@ -112,55 +163,7 @@ public class CustomEditText extends EditText {
             }
 
             if (drawableRight != null) {
-
-                bounds = null;
-                bounds = drawableRight.getBounds();
-
-                int x, y;
-                int extraTapArea = 13;
-
-                /*
-                  IF USER CLICKS JUST OUT SIDE THE RECTANGLE OF THE DRAWABLE
-                  THAN ADD X AND SUBTRACT THE Y WITH SOME VALUE SO THAT AFTER
-                  CALCULATING X AND Y CO-ORDINATE LIES INTO THE DRAWBABLE
-                  BOUND. - this process help to increase the tappable area of
-                  the rectangle.
-                 */
-                x = (int) (actionX + extraTapArea);
-                y = (int) (actionY - extraTapArea);
-
-                /*
-                  Since this is right drawable subtract the value of x from the width
-                  of view. so that width - tappedarea will result in x co-ordinate in drawable bound.
-                 */
-                x = getWidth() - x;
-
-                /*x can be negative if user taps at x co-ordinate just near the width.
-                 * e.g views width = 300 and user taps 290. Then as per previous calculation
-                 * 290 + 13 = 303. So subtract X from getWidth() will result in negative value.
-                 * So to avoid this add the value previous added when x goes negative.
-                 */
-
-                if (x <= 0) {
-                    x += extraTapArea;
-                }
-
-                /* If result after calculating for extra tappable area is negative.
-                 * assign the original value so that after subtracting
-                 * extra tapping area value doesn't go into negative value.
-                 */
-
-                if (y <= 0)
-                    y = actionY;
-
-                // If drawble bounds contains the x and y points then move ahead.
-                if (bounds.contains(x, y) && clickListener != null) {
-                    clickListener
-                            .onClick(DrawableClickListener.DrawablePosition.RIGHT);
-                    event.setAction(MotionEvent.ACTION_CANCEL);
-                    return false;
-                }
-                return super.onTouchEvent(event);
+                return isDrawableRight(bounds, event);
             }
 
         }
